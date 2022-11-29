9c5d7d320e1488f6fdcd7923d2b7fb58ecec39f5
mcakyerima <mcakyerima@gmail.com>
2022-11-30 00:48:13 +0100

    update pages

---
+++ main.js
@@ -0,0 +1,22 @@
+const navItems = document.querySelector('.nav_items');
+const openNavBtn = document.querySelector('#open_nav-btn');
+const closeNavBtn = document.querySelector('#close_nav-btn');
+
+
+const openNav =  () => {
+    navItems.style.display = 'flex'
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block'
+}
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    closeNavBtn.style.display ='none'
+    openNavBtn.style.display = 'inline-block'
+    
+}
+
+
+openNavBtn.addEventListener('click', openNav)
+
+closeNavBtn.addEventListener('click',closeNav)
